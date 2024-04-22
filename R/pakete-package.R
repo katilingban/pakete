bec00137940ac98b921f2266ac253f34f257516a
Ernest Guevarra <ernest@guevarra.io>
2024-04-22 06:40:58 +0100

    initial commit

---
+++ R/pakete-package.R
@@ -0,0 +1,11 @@
+#'
+#' Tools and utilities for package development currently not available from
+#' usual development tools. These are mostly linked to personal preferences
+#' during the development process. They assist in making routine and repetitive
+#' tasks easily implementable.
+#'
+#' @docType package
+#' @keywords internal
+#' @name pakete
+#'
+"_PACKAGE"
