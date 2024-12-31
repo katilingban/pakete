eab0c368c7dcff756c055d2c9bf70bd1406087c8
Ernest Guevarra <ernest@guevarra.io>
2024-12-31 00:38:58 +0000

    add tests for add_github_action; fix #38

--- tests/testthat/test-add_contributing.R
+++ tests/testthat/test-add_contributing.R
@@ -1,8 +1,6 @@
 # Tests for add_contributing functions -----------------------------------------
 
 test_that("add_contributing works as expected", {
-  expect_type(create_contributing(repo = "katilingban/pakete"), "character")
-
   dir.create(".github")
 
   add_contributing(repo = "katilingban/pakete")
@@ -15,3 +13,23 @@ test_that("add_contributing works as expected", {
   file.remove(".github/CONTRIBUTING.md")
   unlink(".github", recursive = TRUE)
 })
+
+
+# test_that("overwrite = TRUE works as expected", {
+#   dir.create(".github")
+
+#   file.copy(
+#     from = system.file("templates", "CONTRIBUTING.md", package = "pakete"),
+#     to = ".github"
+#   )
+
+#   add_contributing(repo = "katilingban/pakete", overwrite = TRUE)
+
+#   expect_snapshot_file(
+#     path = ".github/CONTRIBUTING.md",
+#     name = "CONTRIBUTING.md"
+#   )
+
+#   file.remove(".github/CONTRIBUTING.md")
+#   unlink(".github", recursive = TRUE)
+# })
---
+++ tests/testthat/test-add_github_action.R
@@ -0,0 +1,70 @@
+# Tests for add_github_action functions ----------------------------------------
+
+test_that("add_github_action works as expected", {
+  dir.create(".github/workflows", recursive = TRUE)
+
+  expect_error(add_github_action())
+
+  add_github_action(gha_name = "netlify")
+
+  expect_snapshot_file(
+    path = ".github/workflows/netlify.yaml",
+    name = "netlify.yaml"
+  )
+
+  file.remove(".github/workflows/netlify.yaml")
+  unlink(".github", recursive = TRUE)
+})
+
+
+test_that("add_github_action no directory works as expected", {
+  add_github_action(gha_name = "netlify")
+
+  expect_snapshot_file(
+    path = ".github/workflows/netlify.yaml",
+    name = "netlify.yaml"
+  )
+
+  file.remove(".github/workflows/netlify.yaml")
+  unlink(".github", recursive = TRUE)
+})
+
+
+test_that("add_github_action overwrite = TRUE works as expected", {
+  dir.create(".github/workflows", recursive = TRUE)
+
+  file.copy(
+    from = system.file("actions", "netlify.yaml", package = "pakete"),
+    to = ".github/workflows"
+  )
+
+  add_github_action(gha_name = "netlify", overwrite = TRUE)
+
+  expect_snapshot_file(
+    path = ".github/workflows/netlify.yaml",
+    name = "netlify.yaml"
+  )
+
+  file.remove(".github/workflows/netlify.yaml")
+  unlink(".github", recursive = TRUE)
+})
+
+
+test_that("add_github_action overwrite = FALSE works as expected", {
+  dir.create(".github/workflows", recursive = TRUE)
+
+  file.copy(
+    from = system.file("actions", "netlify.yaml", package = "pakete"),
+    to = ".github/workflows"
+  )
+
+  add_github_action(gha_name = "netlify")
+
+  expect_snapshot_file(
+    path = ".github/workflows/netlify.yaml",
+    name = "netlify.yaml"
+  )
+
+  file.remove(".github/workflows/netlify.yaml")
+  unlink(".github", recursive = TRUE)
+})
