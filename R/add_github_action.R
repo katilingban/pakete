c8e987c8ca7bfc5f5ab1abb26f40fbc8b3bcba87
Ernest Guevarra <ernest@guevarra.io>
2025-03-10 08:08:16 +0000

    remove codeberg mirror actions

--- R/add_github_action.R
+++ R/add_github_action.R
@@ -87,8 +87,7 @@ choose_gha_workflow <- function(gha_name) {
   prompt <- cli::format_inline("Which action do you want to add? (0 to exit)\n")
 
   workflows <- c(
-    "netlify" = "Preview pkgdown website on pull request via Netlify",
-    "mirror-to-codeberg" = "Setup mirroring to Codeberg"
+    "netlify" = "Preview pkgdown website on pull request via Netlify"
   )
   
   options <- paste0(cli::style_bold(names(workflows)), ": ", workflows)
